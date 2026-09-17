57eeb37b25890aae0b096699b2474736c8997c30
Anik <ajaj.anik180@gmail.com>
2026-09-17 17:50:14 +0600

    design home page layout

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("loginButton")
+    .addEventListener("click", function (e) {
+        e.preventDefault()
+
+        const mobileNumber = 01761890561
+        const pinNumber = 1234
+        const mobileNumberValue = document.getElementById("mobile-number").value
+        const mobileNumberValueConverted = parseInt(mobileNumberValue)
+
+        const pinNumbervalue = document.getElementById("pin-number").value
+        const pinNumbervalueConverted = parseInt(pinNumbervalue)
+
+        console.log(mobileNumberValueConverted, pinNumbervalueConverted)
+
+        if (mobileNumberValueConverted === mobileNumber
+            && pinNumbervalueConverted === pinNumber) {
+console.log("All value matched")
+        } else{
+            console.log("invalid credentials") 
+        }
+
+
+    }) 
