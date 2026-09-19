456e42386ea7cd2a784a043f9babbc6579e5a42e
Anik <ajaj.anik180@gmail.com>
2026-09-19 09:09:54 +0600

    code fixed

--- home.js
+++ home.js
@@ -1,19 +1,24 @@
 document.getElementById("add-money-btn")
-    .addEventListener("click",
-        function (e) {
-            e.preventDefault
-            console.log('add money btn clicked')
-            const bank = document.getElementById("bank").value
-            const accountNumber = document.getElementById("account-number").value
-            const amount = parseInt(document.getElementById("add-amount").value)
+    .addEventListener("click", function (e) {
 
-            const pin = document.getElementById("add-pin").value
-            console.log(bank, accountNumber, amount, pin);
-const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+        e.preventDefault();
 
-console.log(available-balance)
+        console.log("add money btn clicked");
 
-const totalNewAvailableBalance = amount+ availableBalance
-document.getElementById("available-balance")
-.innerText = totalNewAvailableBalance
-        })
+        const bank = document.getElementById("bank").value;
+        const accountNumber = document.getElementById("account-number").value;
+        const amount = parseInt(document.getElementById("add-amount").value);
+        const pin = document.getElementById("add-pin").value;
+
+        console.log(bank, accountNumber, amount, pin);
+
+        const availableBalance =
+            parseInt(document.getElementById("available-balance").innerText);
+
+        console.log(availableBalance);
+
+        const totalNewAvailableBalance = amount + availableBalance;
+
+        document.getElementById("available-balance").innerText =
+            totalNewAvailableBalance;
+    });
