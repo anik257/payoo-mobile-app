14b228e2ce1fe39cdde6657dbc0118e880aab2af
Anik <ajaj.anik180@gmail.com>
2026-09-19 17:39:15 +0600

    Implement Toggle Feature

--- home.js
+++ home.js
@@ -23,14 +23,35 @@ document.getElementById("add-money-btn")
             return;
         }
 
-if (pin !==  validPin){
-    alert("please valid pin number")
-    return;
+        if (pin !== validPin) {
+            alert("please valid pin number")
+            return;
 
-}
+        }
 
         const totalNewAvailableBalance = amount + availableBalance;
 
         document.getElementById("available-balance").innerText =
             totalNewAvailableBalance;
     });
+
+
+// toggling feature
+
+
+document.getElementById("add-button")
+    .addEventListener("click", function () {
+        document.getElementById("cash-out-parent").style.display = "none"
+        document.getElementById("add-money-parent").style.display = "block"
+
+
+    })
+document.getElementById("cash-out-button")
+    .addEventListener("click", function () {
+
+        document.getElementById("add-money-parent").style.display = "none"
+        document.getElementById("cash-out-parent").style.display = "block"
+
+
+
+    })
